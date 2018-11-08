51e1a15e1036bf96402eb380a9cea4f5a2d07e0b
Christophe Gisquet <christophe.gisquet@crf.canon.fr>
2018-11-08 11:36:49 +0100

    x86: splat_mv: Duff device
    Reduce code size from 5xx to 301. Few cycles loss.

--- src/x86/splat_mv.asm
+++ src/x86/splat_mv.asm
@@ -1,54 +1,43 @@
 %include "config.asm"
 %include "ext/x86/x86inc.asm"
 
+SECTION_RODATA 32
+
+%macro JMP_TABLE 1-*
+    %xdefine %1_table (%%table - 2*%2)
+    %xdefine %%base %1_table
+    %xdefine %%prefix mangle(private_prefix %+ _%1)
+    %%table:
+    %rep %0 - 1
+        dd %%prefix %+ .loop%2 - %%base
+        %rotate 1
+    %endrep
+%endmacro
+
+JMP_TABLE splat_mv_sse, 4, 8, 16, 32
+
 SECTION .text
 
 INIT_XMM sse
 ; refmvs *r, ptrdiff_t stride, int bw4, int bh4, aliasmv *a
 cglobal splat_mv, 5, 5, 3, r, stride, bw4, bh4, a
-    movzx         bw4q, bw4w
-    movzx         bh4q, bh4w
     imul       strideq, 12
-    cmp           bw4d, 1
-    jle        .start1
-    cmp           bw4d, 2
+    cmp           bw4q, 2
     jle        .start2
 
+.dummy:
+    tzcnt         bw4d, bw4m
     mova            m0, [aq+0]
     mova            m1, m0
     mova            m2, m0
+%define base aq-width_sse_table
+    lea             aq, [splat_mv_sse_table]
+    movsxd        bw4q, dword [aq+bw4q*4]
+    add           bw4q, aq
     shufps      m0, m0, q0210
     shufps      m1, m1, q1021
     shufps      m2, m2, q2102
-    cmp           bw4d, 32
-    jge        .loop32
-    cmp           bw4d, 4
-    jle         .loop4
-
-    shr           bh4q, 1
-    cmp           bw4d, 16
-    jge        .loop16
-
-.loop8: ; 2 rows at a time
-    mova     [rq+  80], m2
-    mova     [rq+  64], m1
-    mova     [rq+  48], m0
-;.loop4:
-    mova     [rq+  32], m2
-    mova     [rq+  16], m1
-    mova     [rq+   0], m0
-
-    mova     [rq+ strideq+ 80], m2
-    mova     [rq+ strideq+ 64], m1
-    mova     [rq+ strideq+ 48], m0
-;.loop4:
-    mova     [rq+ strideq+ 32], m2
-    mova     [rq+ strideq+ 16], m1
-    mova     [rq+ strideq+  0], m0
-    lea             rq, [rq+2*strideq]
-    dec           bh4q
-    jnz         .loop8
-    RET
+    jmp           bw4q
 
 .loop32:
     mova     [rq+ 368], m2
@@ -63,84 +52,31 @@ cglobal splat_mv, 5, 5, 3, r, stride, bw4, bh4, a
     mova     [rq+ 224], m2
     mova     [rq+ 208], m1
     mova     [rq+ 192], m0
-;.loop16:
+.loop16:
     mova     [rq+ 176], m2
     mova     [rq+ 160], m1
     mova     [rq+ 144], m0
     mova     [rq+ 128], m2
     mova     [rq+ 112], m1
     mova     [rq+  96], m0
-;.loop8:
+.loop8:
     mova     [rq+  80], m2
     mova     [rq+  64], m1
     mova     [rq+  48], m0
-;.loop4:
-    mova     [rq+  32], m2
-    mova     [rq+  16], m1
-    mova     [rq+   0], m0
-    add             rq, strideq
-    dec           bh4q
-    jge        .loop32
-    RET
-
-.loop16: ; 2 rows at a time
-    mova     [rq+ 176], m2
-    mova     [rq+ 160], m1
-    mova     [rq+ 144], m0
-    mova     [rq+ 128], m2
-    mova     [rq+ 112], m1
-    mova     [rq+  96], m0
-;.loop8:
-    mova     [rq+  80], m2
-    mova     [rq+  64], m1
-    mova     [rq+  48], m0
-;.loop4:
-    mova     [rq+  32], m2
-    mova     [rq+  16], m1
-    mova     [rq+   0], m0
-    add             rq, strideq
-
-    mova     [rq+ 176], m2
-    mova     [rq+ 160], m1
-    mova     [rq+ 144], m0
-    mova     [rq+ 128], m2
-    mova     [rq+ 112], m1
-    mova     [rq+  96], m0
-;.loop8:
-    mova     [rq+  80], m2
-    mova     [rq+  64], m1
-    mova     [rq+  48], m0
-;.loop4:
-    mova     [rq+  32], m2
-    mova     [rq+  16], m1
-    mova     [rq+   0], m0
-    add             rq, strideq
-    dec           bh4q
-    jnz        .loop16
-    RET
-
 .loop4:
     mova     [rq+  32], m2
     mova     [rq+  16], m1
     mova     [rq+   0], m0
     add             rq, strideq
     dec           bh4q
-    jnz         .loop4
-    RET
-
-.start1:
-    movlps          m0, [aq]
-    mov           bw4d, dword [aq+8]
-.loop1:
-    movlps    [rq+  0], m0
-    mov dword [rq+  8], bw4d
-    add             rq, strideq
-    dec           bh4q
-    jnz         .loop1
+    jz            .end
+    jmp           bw4q
+.end:
     RET
 
 .start2:
-    mova            m0, [aq+0]
+    cmp           bw4q, 1
+    jle        .start1
     mova            m0, [aq+0]
     mova            m1, m0
     shufps      m0, m0, q0210
@@ -152,4 +88,15 @@ cglobal splat_mv, 5, 5, 3, r, stride, bw4, bh4, a
     add             rq, strideq
     dec           bh4q
     jnz         .loop2
+    RET
+
+.start1:
+    movlps          m0, [aq]
+    mov           bw4d, dword [aq+8]
+.loop1:
+    movlps    [rq+  0], m0
+    mov dword [rq+  8], bw4d
+    add             rq, strideq
+    dec           bh4q
+    jnz         .loop1
     RET
